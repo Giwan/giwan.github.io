d22e2cf11573f4643d847fb0f6bcd28459830945
Giwan Persaud <giwan.persaud@gmail.com>
2025-06-29 14:22:01 +0200

    fix: color highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
     outDir: "./docs",
     markdown: {
         shikiConfig: {
-            theme: 'github-dark',
+            theme: 'monokai',
             wrap: true
         }
     }
