5b1d264d9ec0244de33343b0d4238ee84eb4f225
Giwan Persaud <giwan.persaud@gmail.com>
2025-07-13 17:18:02 +0200

    fix: contact and date

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Giwan's blog";
-export const SITE_DESCRIPTION = "Front-end dev articles";
+export const SITE_DESCRIPTION = "Exploring code & technology";
+export const SITE_DATE = new Date().toISOString().split("T")[0];
